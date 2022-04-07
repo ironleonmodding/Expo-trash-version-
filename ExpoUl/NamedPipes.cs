33b06e50b3efb57a5f8f5cf1d805be8597c2184a
ironleonmodding <96918794+ironleonmodding@users.noreply.github.com>
2022-04-07 20:16:54 +0800

    Add files via upload

---
+++ ExpoUl/NamedPipes.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace IceMemeUI
+{
+    class NamedPipes
+    {
+        public static string cmdpipename = "ExpCmd";//name of command pipe
+        public static string luacpipename = "ExpoLuaC";// name of lua c pipe
+        public static string luapipename = "ExpoLua";//name of lua pipe
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool WaitNamedPipe(string name, int timeout);
+        //function to check if the pipe exist
+        public static bool NamedPipeExist(string pipeName)
+        {
+            bool result;
+            try
+            {
+                int timeout = 0;
+                if (!WaitNamedPipe(Path.GetFullPath(string.Format("\\\\\\\\.\\\\pipe\\\\{0}", pipeName)), timeout))
+                {
+                    int lastWin32Error = Marshal.GetLastWin32Error();
+                    if (lastWin32Error == 0)
+                    {
+                        result = false;
+                        return result;
+                    }
+                    if (lastWin32Error == 2)
+                    {
+                        result = false;
+                        return result;
+                    }
+                }
+                result = true;
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+        //command pipe function
+        public static void CommandPipe(string command)
+        {
+            if (NamedPipeExist(cmdpipename))
+            {
+                new Thread(() =>//lets run this in another thread so if roblox crash the ui/gui don't freeze or something
+                {
+                    try
+                    {
+                    using (NamedPipeClientStream namedPipeClientStream = new NamedPipeClientStream(".", cmdpipename, PipeDirection.Out))
+                    {
+                        namedPipeClientStream.Connect();
+                        using (StreamWriter streamWriter = new StreamWriter(namedPipeClientStream))
+                        {
+                            streamWriter.Write(command);
+                            streamWriter.Dispose();
+                        }
+                        namedPipeClientStream.Dispose();
+                    }
+                    }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error occured connecting to the pipe.", "Connection Failed!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+                }).Start();
+            }
+            else
+            {
+                MessageBox.Show("Inject " + Functions.exploitdll + " before Using this!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+        }
+        //lua c pipe function
+        public static void LuaCPipe(string script)
+        {
+            if (NamedPipeExist(luacpipename))
+            {
+                new Thread(() =>//lets run this in another thread so if roblox crash the ui/gui don't freeze or something
+                {
+                    try
+                    {
+                        using (NamedPipeClientStream namedPipeClientStream = new NamedPipeClientStream(".", luacpipename, PipeDirection.Out))
+                        {
+                            namedPipeClientStream.Connect();
+                            using (StreamWriter streamWriter = new StreamWriter(namedPipeClientStream, System.Text.Encoding.Default, 999999))//changed buffer to max 1mb since default buffer is 1kb
+                            {
+                                streamWriter.Write(script);
+                                streamWriter.Dispose();
+                            }
+                            namedPipeClientStream.Dispose();
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error occured connecting to the pipe.", "Connection Failed!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }).Start();
+            }
+            else
+            {
+                MessageBox.Show("Inject " + Functions.exploitdll + " before Using this!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+        }
+
+        //lua pipe function
+        public static void LuaPipe(string script)
+        {
+            if (NamedPipeExist(luapipename))
+            {
+                new Thread(() =>//lets run this in another thread so if roblox crash the ui/gui don't freeze or something
+                {
+                    try
+                    {
+                        using (NamedPipeClientStream namedPipeClientStream = new NamedPipeClientStream(".", luapipename, PipeDirection.Out))
+                        {
+                            namedPipeClientStream.Connect();
+                            using (StreamWriter streamWriter = new StreamWriter(namedPipeClientStream, System.Text.Encoding.Default, 999999))//changed buffer to max 1mb since default buffer is 1kb
+                            {
+                                streamWriter.Write(script);
+                                streamWriter.Dispose();
+                            }
+                            namedPipeClientStream.Dispose();
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error occured connecting to the pipe.", "Connection Failed!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }).Start();
+            }
+            else
+            {
+                MessageBox.Show("Inject " + Functions.exploitdll + " before Using this!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+        }
+    }
+}
---
+++ ExpoUl/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace ExpoAlphaUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Checks();
+            VersionCheck();
+        }
+
+        private static void Checks()
+        {
+            try
+            {
+                if (!Directory.Exists("./LuaScripts"))
+                {
+                    Directory.CreateDirectory("LuaScripts");
+                }
+                if (!Directory.Exists("./LuaCScripts"))
+                {
+                    Directory.CreateDirectory("LuaCScripts");
+                }
+                if (!File.Exists("./" + Functions.exploitdll))
+                {
+                    MessageBox.Show(Functions.exploitdll + " not found", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Environment.Exit(0);
+                }
+                if (!File.Exists("./FastColoredTextBox.dll"))
+                {
+                    MessageBox.Show("FastColoredTextBox.dll not found", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Environment.Exit(0);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+                Environment.Exit(0);
+            }
+        }
+
+        private static void VersionCheck()
+        {
+            try
+            {
+                if (Properties.Settings.Default.CheckUpdates)
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        string UIWebVersion = client.DownloadString("https://rakion99.github.io/IceMeme/Version.txt");
+                        string UICurrentVerion = Application.ProductVersion;
+                        if (UIWebVersion != UICurrentVerion)
+                        {
+                            string UIUpdateFound = string.Format("An update is available\nYour Current version is: {0}\nNew Version is: {1}\n\nDo you want to update?", UICurrentVerion, UIWebVersion);
+                            DialogResult UIUpdaterChecker = MessageBox.Show(UIUpdateFound, "New Update Found", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                            if (UIUpdaterChecker == DialogResult.Yes)
+                            {
+                                Functions.UpdateUI = true;
+                                new UpdaterForm().ShowDialog();
+                            }
+                        }
+                        string DLLCurrentHASH = Getsha256("./" + Functions.exploitdll);
+                        string DLLWebHASH = client.DownloadString("https://rakion99.github.io/IceMeme/DLLHash.txt");
+                        if (DLLWebHASH != DLLCurrentHASH)
+                        {
+                            DialogResult DLLUpdaterChecker = MessageBox.Show("An update for the dll is available\n\nDo you want to update?", "New Update Found", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                            if (DLLUpdaterChecker == DialogResult.Yes)
+                            {
+                                Functions.UpdateDLL = true;
+                                new UpdaterForm().ShowDialog();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            Application.Run(new IceMemeForm());
+        }
+
+        private static string Getsha256(string filepath)
+        {
+            FileStream filestream;
+            filestream = new FileStream(filepath, FileMode.Open)
+            {
+                Position = 0
+            };
+
+            return BitConverter.ToString(SHA256.Create().ComputeHash(filestream)).Replace("-", string.Empty);
+        }
+    }
+}
